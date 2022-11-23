f239823306e09815770aaab63b02606cf2993a86
Kent <kentencroth@yahoo.com>
2022-11-23 11:51:06 -0600

    Changes to axios config

---
+++ src/axios/axiosProject.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const token = localStorage.getItem('projectToken');
+
+const instance = axios.create({
+	baseURL: 'http://localhost:3000',
+	headers: {
+		Authorization: `Bearer ${token}`,
+	},
+});
+
+export default instance;

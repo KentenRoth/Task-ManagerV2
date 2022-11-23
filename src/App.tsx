1f29c6124c1aeab2fffa6ed0bd90f64a1bb4e42f
Kent <kentencroth@yahoo.com>
2022-11-23 11:51:24 -0600

    Adding new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './sass/app.sass';
 
 // Route Components
 import Login from './Pages/login';
 import SignUp from './Pages/signUp';
+import TaskManager from './Pages/project';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Login />} />
+					<Route path="/" element={<TaskManager />} />
+					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<SignUp />} />
 				</Routes>
 			</BrowserRouter>

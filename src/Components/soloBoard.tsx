479a596860b07a97ec76925e06a1c24205257f45
Kent <kentencroth@yahoo.com>
2023-07-04 07:11:39 -0500

    Updated for DnD

--- src/Components/soloBoard.tsx
+++ src/Components/soloBoard.tsx
@@ -1,7 +1,8 @@
-import SoloTickets from '../Pages/soloTickets';
 import { RootState } from '../app/store';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import Columns from './columns';
+import axiosProject from '../axios/axiosProject';
 
 interface Tickets {
 	_id: string;
@@ -16,56 +17,109 @@ interface Tickets {
 	completed: boolean;
 	assigned: boolean;
 	currentFocus: boolean;
+	order: number;
+}
+
+interface AllColumns {
+	title: string;
+	tickets: Tickets[];
 }
 
 const SoloBoard = () => {
 	const allTickets = useSelector((state: RootState) => state.tickets);
-	const [tickets, setTickets] = useState<Tickets[]>([]);
-	const [completed, setCompleted] = useState<Tickets[]>([]);
-	const [unCompleted, setUncompleted] = useState<Tickets[]>([]);
+	const [columns, setColumns] = useState<AllColumns[]>([]);
+	const [isLoaded, setLoaded] = useState<boolean>(false);
 
 	useEffect(() => {
 		sortTickets(allTickets.tickets);
 	}, [allTickets]);
 
+	useEffect(() => {
+		if (columns.length === 0) {
+			return;
+		}
+		if (!isLoaded) {
+			return setLoaded(true);
+		}
+
+		sendingNewTicketOrder();
+	}, [columns]);
+
 	let sortTickets = (array: Tickets[]) => {
-		const ticketGroups: {
-			[key: string]: Tickets[];
-		} = {
-			completed: [],
-			currentFocus: [],
-			unCompleted: [],
-		};
+		if (array.length === 0) {
+			return;
+		}
+		let completed: Tickets[] = [];
+		let currentFocus: Tickets[] = [];
+		let tasks: Tickets[] = [];
 
 		array.forEach((ticket) => {
 			if (ticket.completed) {
-				return ticketGroups.completed.push(ticket);
+				return completed.push(ticket);
 			}
 			if (ticket.currentFocus) {
-				return ticketGroups.currentFocus.push(ticket);
+				return currentFocus.push(ticket);
 			}
-
-			return ticketGroups.unCompleted.push(ticket);
+			return tasks.push(ticket);
 		});
 
-		setCompleted(ticketGroups.completed);
-		setUncompleted(ticketGroups.unCompleted);
-		setTickets([...ticketGroups.currentFocus]);
+		completed.sort((a, b) => a.order - b.order);
+		currentFocus.sort((a, b) => a.order - b.order);
+		tasks.sort((a, b) => a.order - b.order);
+
+		setColumns([
+			{ title: 'CurrentFocus', tickets: currentFocus },
+			{ title: 'Tasks', tickets: tasks },
+			{ title: 'Completed', tickets: completed },
+		]);
+	};
+
+	let afterDrop = (update: Tickets[], column: number) => {
+		setColumns((prevState) => {
+			const updated = [...prevState];
+			updated[column] = {
+				...updated[column],
+				tickets: update,
+			};
+			return updated;
+		});
+		console.log(column);
+	};
+
+	let sendingNewTicketOrder = () => {
+		if (columns.length === 0) {
+			return;
+		}
+		let allTicketIds: string[] = [];
+		columns.forEach((column) => {
+			column.tickets.forEach((ticket) => {
+				allTicketIds.push(ticket._id);
+			});
+		});
+
+		axiosProject
+			.patch('/tickets/reorder', {
+				ticketIds: allTicketIds,
+			})
+			.then((response) => {
+				console.log(response);
+			});
 	};
 
 	return (
 		<>
-			<div className="solo">
-				<SoloTickets
-					tickets={tickets.filter((ticket) => ticket.currentFocus)}
-				/>
-			</div>
-			<div>
-				<SoloTickets tickets={unCompleted} />
-			</div>
-			<div>
-				<SoloTickets tickets={completed} />
-			</div>
+			{columns.map((column, index) => {
+				return (
+					<div className="columns_container" key={index}>
+						<Columns
+							title={column.title}
+							tickets={column.tickets}
+							afterDrop={afterDrop}
+							index={index}
+						/>
+					</div>
+				);
+			})}
 		</>
 	);
 };
